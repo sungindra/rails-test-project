62d5a0d84dfcde3b3dc945db693a47ec2df8773c
sungindra <sungindra.setiawan@gmail.com>
2019-03-15 14:35:29 +0700

    View Helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def test_helper
+    "<p>TEST HELPER</p>".html_safe
+  end
+
+  def auth_bar_helper
+    if current_user.is_a? User
+      link_to "LogOut", destroy_user_session_path, method: :delete
+    else
+      (link_to "LogIn", new_user_session_path) +
+      "<br>".html_safe +
+      (link_to "Register", new_user_registration_path)
+    end
+  end
 end
