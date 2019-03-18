4966676d1be269d02352d3ea6c98eeebbcd6abf3
sungindra <sungindra.setiawan@gmail.com>
2019-03-18 16:19:56 +0700

    Hello message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,10 @@ module ApplicationHelper
 
   def auth_bar_helper
     if current_user.is_a? User
-      link_to "LogOut", destroy_user_session_path, method: :delete
+      "<p>Hello, #{current_user.last_name}  </p> ".html_safe +
+      (link_to "Logout", destroy_user_session_path, method: :delete)
     else
-      (link_to "LogIn", new_user_session_path) +
+      (link_to "Login", new_user_session_path) +
       "<br>".html_safe +
       (link_to "Register", new_user_registration_path)
     end
