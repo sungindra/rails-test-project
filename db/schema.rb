0125ce14b1f9c0ce6af82514f5e5ff1f3c415ccd
sungindra <sungindra.setiawan@gmail.com>
2019-03-18 10:24:53 +0700

    Added position to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_15_034156) do
+ActiveRecord::Schema.define(version: 2019_03_18_032032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_03_15_034156) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.integer "status", default: 0
+    t.integer "position"
     t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
